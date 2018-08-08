b2d9df20f1d47be3e29c1ce671ab98ff18606da5
christian8aguirre <christian8.aguirre@icloud.com>
2018-08-08 10:07:40 -0500

    d27_dribbble--create-basic-structure-part-1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.css';
+import Header from './resources/header.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/resources/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Header extends React.Component{
+    render() {
+      return (
+        <div className='App__header'>
+          <div className="App__header--head">
+          sdasdas
+          </div>
+          <div className="App__header--body">
+          
+          </div>
+          <div className="App__header--foot">
+          sfdgd
+          </div>
+        </div>
+      )
+    }
+}
+
+export default Header
